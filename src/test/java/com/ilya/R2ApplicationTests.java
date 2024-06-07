711d23f776853665c1f75d8a898162da5f32b41f
ilya2012gvy <ilea.morozov.2003@bk.ru>
2024-06-07 22:53:23 +0300

    1

---
+++ src/test/java/com/ilya/R2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
